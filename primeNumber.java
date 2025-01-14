ee14a69d8d0aa842f88b11ce671cf9520422eab8
unknown <sivaflutterdeveloper@gmail.com>
2025-01-14 21:55:28 +0530

    prime numbers

---
+++ primeNumber.java
@@ -0,0 +1,23 @@
+public class primeNumber {
+    public static void main(String[] args) {
+
+
+        for (int i = 2; i <= 100; i++) {
+            String isPrimeOrNot = isPrime(i);
+            System.out.println(isPrimeOrNot);
+        }
+    }
+
+    public static String isPrime(int number) {
+        if (number <= 1) {
+            return number+" Not A Prime Number";
+        }
+        for (int i = 2; i < number / 2; i++) {
+            if (number % i == 0) {
+                return number+" Not A Prime Number";
+            }
+        }
+
+        return number+" It Is A Prime Number";
+    }
+}
